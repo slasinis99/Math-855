fde2bb7b6333e55b28aeefcfcd7837a5ffb4b4fc
Stephen Lasinis <recon589@gmail.com>
2024-09-06 08:59:30 -0500

    added latex conversion

--- topologies.py
+++ topologies.py
@@ -46,8 +46,28 @@ def all_complements_n_elements(n: int):
         c_list.append(complements_of_topology(X, t))
     return c_list
 
-c_list = all_complements_n_elements(3)
+# Latex conversion for 2.5
+c_list = all_topology_n_elements(3)
 print(f'Number of 3-element topologies = {len(c_list)}')
 print(f'Here is the list of all collections of subsets of a 3-elements set which are collections of closed sets for some topology:')
+d = {1:'a', 2:'b', 3:'c'}
 for c in c_list:
-    print(c)
+    l = [set()]
+    el = sorted(c, key=lambda x: len(x))
+    el = el[1:len(el)]
+    el.sort(key=lambda x: (len(x), next(iter(x))))
+    l.extend(el)
+    
+    s = '&\\{'
+    for e in l:
+        if len(e) == 0:
+            s += '\\emptyset, '
+        else:
+            s += ' \\{'
+            for i in e:
+                s += f'{d[i]},'
+            s = s[0:len(s)-1]
+            s += '\\}, '
+    s = s[0:len(s)-2]
+    s += '\\} \\\\'
+    print(s)
